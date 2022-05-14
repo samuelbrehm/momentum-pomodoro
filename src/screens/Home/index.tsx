e5e980a1c00135c859f7cfe8e20505646c556a2b
Samuel Brehm <samuelbrehm@gmail.com>
2022-05-14 15:41:34 -0300

    Setup ESLint and prettier

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export function Home() {
-  return(
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'blue'
-    }}>
-      <Text style={{
-        fontSize: 20,
-        color: '#fff'
-      }}>Home Screen</Text>
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'blue',
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 20,
+          color: '#fff',
+        }}
+      >
+        Home Screen
+      </Text>
     </View>
-  )
+  );
 }
