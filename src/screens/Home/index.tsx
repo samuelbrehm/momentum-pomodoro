cc4f102434b8dbcaf60be06cc5d326b4a5425432
Samuel Brehm <samuelbrehm@gmail.com>
2022-05-15 21:14:48 -0300

    Add lib react-native-vector-icons and folder Fonts into XCode

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 export function Home() {
   return (
     <View
@@ -8,7 +10,7 @@ export function Home() {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'blue',
+        backgroundColor: 'yellow',
       }}
     >
       <Text
@@ -19,6 +21,9 @@ export function Home() {
       >
         Home Screen
       </Text>
+
+      <Icon name="users" size={60} color="#900" />
+      <Icon name="rocket" size={60} color="#900" />
     </View>
   );
 }
