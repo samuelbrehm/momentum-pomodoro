d187bcc51a4e073f872c154fd483cc4be48e3134
Samuel Brehm <samuelbrehm@gmail.com>
2022-05-21 17:02:17 -0300

    Setup import SVG files components react

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import styled from 'styled-components/native';
-
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import AddTasks from '../../resources/assets/add_tasks.svg';
 
 export function Home() {
   return (
     <Container>
+      <ImageHome />
       <Title>Home Screen</Title>
 
       <Icon name="users" size={60} color="#F22E63" />
@@ -20,9 +23,18 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.colors.background};
+  padding-left: 24px;
+  padding-right: 24px;
 `;
 
 const Title = styled.Text`
   font-size: 25px;
   color: #fff;
+`;
+
+const ImageHome = styled(AddTasks).attrs({
+  width: '100%',
+  height: `${RFValue(200)}`,
+})`
+  margin-bottom: 48px;
 `;
